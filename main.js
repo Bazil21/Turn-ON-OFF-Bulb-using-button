ba94a6f892a233645dcb7101aa8e4e8f7e232aba
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-13 00:07:05 +0500

    Turn On and Off bulb using javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+var on = document.getElementById("ON");
+var off = document.getElementById("OFF");
+var img = document.getElementById("img");
+
+function ON(){
+   img.src = "./Images/ONbulb.jpg"
+} 
+
+function OFF(){
+    img.src = "./Images/OFFbulb (1).jpg"
+ } 
